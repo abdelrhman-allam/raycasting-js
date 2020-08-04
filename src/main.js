b9b6794ce5973da5343cda2db4078ca3b7cd375c
Abdelrhman Mohamed Allam <abdelrhman.allam@gmail.com>
2020-08-04 22:32:03 +0200

    Fix Depth Ligthing

--- src/main.js
+++ src/main.js
@@ -161,11 +161,12 @@ function rayCasting(px, py, pangle) {
       let dx = parseInt(parseInt(x / m.tileSize) * m.tileSize);
       //console.log(dy, dx);
       if (m.worldMap.some((c) => c[0] == dy && c[1] == dx)) {
-        let color = parseInt(255 / (1 + depth * depth * 0.001));
+        let color = parseInt(255 / (1 + depth * depth * 0.0001));
         depth = depth * Math.cos(pangle - current_angle);
         let projHeight = PROJ_COEFF / depth;
 
-        ctx3d.fillStyle = `#${color}${color}${color / 3}`;
+        ctx3d.fillStyle = `rgba(${color / 3},${color},${color / 2},1)`;
+
         ctx3d.fillRect(
           ray * SCALE,
           HALF_HEIGHT - projHeight / 2,
